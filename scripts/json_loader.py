84a63024d576889ac5b5868aac688fe5df91ab2a
David Felce <davefelce@gmail.com>
2017-07-22 09:25:30 +0100

    WHAT-12: Added json_loader and fixtures

---
+++ scripts/json_loader.py
@@ -0,0 +1,22 @@
+from os import listdir
+from os.path import isfile, join
+import json
+import requests
+
+if __name__ == '__main__':
+
+    path = '../fixtures'
+    json_files = [join(path, f) for f in listdir(path) if isfile(join(path, f))]
+
+    for json_file in json_files:
+        with open(json_file) as f:
+            data = json.load(f)
+
+        for row_dict in data:
+            print("doing " + json.dumps(row_dict))
+            url = 'http://localhost:8002/recipes/list/'
+            r = requests.post(url, json=row_dict)
+            print(r.status_code)
+
+
+
