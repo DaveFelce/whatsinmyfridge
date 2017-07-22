7c4d9142879e13ff4c7ffa36158394c9fbe75c88
David Felce <davefelce@gmail.com>
2017-07-22 10:13:30 +0100

    WHAT-12: Code cleanup and refactor

--- common/tests.py
+++ common/tests.py
@@ -1,8 +1,15 @@
 from django.test import TestCase
-from common.utils import CommonUtils
+from common.utils import cleanup_ingredients, lc_list_of_ingredients, sorted_ingredients_as_csv
 
 class UtilsTests(TestCase):
     """ Test common utils """
+
+    def setUp(self):
+        self.ingredients1 = '"one. thing",  two, \'three\',four'
+        self.ingredients2 = '"%%% one.  .. & ^ ^^( thing) **** *%   % $£ !",  two, \'three\',four***'
+        self.ingredients3 = '"""one. \'  thing",  " ;;;;  ,,  ;, ; . two, :\'three\',four:";'
+        self.ingredients4 = 'one thing two three four'
+        self.ingredients5 = 'one  thing   two     three four'
 
     def test_ingredients_cleanup(self):
         """ Test the removal of non-word and extra spaces from ingredients keywords
@@ -10,12 +17,9 @@ class UtilsTests(TestCase):
 
         expected_ingredient_keywords = 'one thing two three four'
 
-        ingredients = '"one. thing",  two, \'three\',four'
-        self.assertEqual(CommonUtils.cleanup_ingredients(ingredients), expected_ingredient_keywords)
-        ingredients = '"%%% one.  .. & ^ ^^( thing) **** *%   % $£ !",  two, \'three\',four***'
-        self.assertEqual(CommonUtils.cleanup_ingredients(ingredients), expected_ingredient_keywords)
-        ingredients = '"""one. \'  thing",  " ;;;;  ,,  ;, ; . two, :\'three\',four:";'
-        self.assertEqual(CommonUtils.cleanup_ingredients(ingredients), expected_ingredient_keywords)
+        self.assertEqual(cleanup_ingredients(self.ingredients1), expected_ingredient_keywords)
+        self.assertEqual(cleanup_ingredients(self.ingredients2), expected_ingredient_keywords)
+        self.assertEqual(cleanup_ingredients(self.ingredients3), expected_ingredient_keywords)
 
     def test_lc_list_of_ingredients(self):
         """ Test the creation of a lowercased list of ingredients keywords
@@ -23,12 +27,9 @@ class UtilsTests(TestCase):
 
         expected_ingredient_keywords_list = ['one', 'thing', 'two', 'three', 'four']
 
-        ingredients = '"%%% one.  .. & ^ ^^( thing) **** *%   % $£ !",  two, \'three\',four***'
-        self.assertListEqual(CommonUtils.lc_list_of_ingredients(ingredients), expected_ingredient_keywords_list)
-        ingredients = 'one  thing   two     three four'
-        self.assertListEqual(CommonUtils.lc_list_of_ingredients(ingredients), expected_ingredient_keywords_list)
-        ingredients = 'one thing two three four'
-        self.assertListEqual(CommonUtils.lc_list_of_ingredients(ingredients), expected_ingredient_keywords_list)
+        self.assertListEqual(lc_list_of_ingredients(self.ingredients2), expected_ingredient_keywords_list)
+        self.assertListEqual(lc_list_of_ingredients(self.ingredients5), expected_ingredient_keywords_list)
+        self.assertListEqual(lc_list_of_ingredients(self.ingredients4), expected_ingredient_keywords_list)
 
     def test_sorted_ingredients_as_csv(self):
         """ Test the creation of a sorted csv string from whitespace separated string
@@ -36,8 +37,6 @@ class UtilsTests(TestCase):
 
         expected_ingredients_string = 'four, one, thing, three, two'
 
-        ingredients = 'one thing two three four'
-        self.assertEqual(CommonUtils.sorted_ingredients_as_csv(ingredients), expected_ingredients_string)
-        ingredients = '"%%% one.  .. & ^ ^^( thing) **** *%   % $£ !",  two, \'three\',four***'
-        self.assertEqual(CommonUtils.sorted_ingredients_as_csv(ingredients), expected_ingredients_string)
+        self.assertEqual(sorted_ingredients_as_csv(self.ingredients4), expected_ingredients_string)
+        self.assertEqual(sorted_ingredients_as_csv(self.ingredients2), expected_ingredients_string)
 
--- common/utils.py
+++ common/utils.py
@@ -3,21 +3,23 @@ import re
 regex_whitespace = re.compile(r'\s+')
 regex_non_words = re.compile(r'\W')
 
-class CommonUtils:
 
-    def cleanup_ingredients(ingredients):
-        ingredients = regex_non_words.sub(' ', ingredients)
-        cleaned_ingredients = ' '.join(regex_whitespace.split(ingredients)).strip()
-        return cleaned_ingredients
+def cleanup_ingredients(ingredients):
+    ''' Remove non-word chars and produce list of words separated by single whitespace '''
+    ingredients = regex_non_words.sub(' ', ingredients)
+    cleaned_ingredients = ' '.join(regex_whitespace.split(ingredients)).strip()
+    return cleaned_ingredients
 
-    def lc_list_of_ingredients(ingredients):
-        # Get ingredient keywords into known shape, separated by single whitespace
-        ingredients = CommonUtils.cleanup_ingredients(ingredients)
-        # We know the words are separated by a single whitespace, so split on that
-        # lowercase each word and return the list
-        return [qw.lower() for qw in regex_whitespace.split(ingredients)]
+def lc_list_of_ingredients(ingredients):
+    ''' Lower case version of cleanup_ingredients() for comparisons '''
+    # Get ingredient keywords into known shape, separated by single whitespace
+    ingredients = cleanup_ingredients(ingredients)
+    # We know the words are separated by a single whitespace, so split on that
+    # lowercase each word and return the list
+    return [qw.lower() for qw in regex_whitespace.split(ingredients)]
 
-    def sorted_ingredients_as_csv(ingredients):
-        ingredients = CommonUtils.lc_list_of_ingredients(ingredients)
-        ingredients = ', '.join(sorted(ingredients))
-        return ingredients
+def sorted_ingredients_as_csv(ingredients):
+    ''' comma separated, stringified version of lc_list_of_ingredients() '''
+    ingredients = lc_list_of_ingredients(ingredients)
+    ingredients = ', '.join(sorted(ingredients))
+    return ingredients
