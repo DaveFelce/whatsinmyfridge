cd904802fca8a82be2b0d351ab6f7af5789ea9f4
David Felce <davefelce@gmail.com>
2017-07-22 14:25:26 +0100

    WHAT-13: More tests for percentage calc function (not a method now)

--- search/views.py
+++ search/views.py
@@ -8,6 +8,40 @@ from pygal.style import CleanStyle
 from services.es_search import RecipeSearch
 from common.utils import lc_list_of_ingredients, sorted_ingredients_as_csv
 
+def percentage_of_ingredients_matched(query_params_ingredients):
+    '''
+    Method to calculate the percentage of ingredient keywords matched for each Elasticsearch hit.
+    Return a closure to cache some calculated values (from query params, which are the same for each call)
+
+    :param query_params_ingredients(str), of space delimited keywords from the user's search:
+    :return: get_percentage_matched (function)
+    '''
+
+    # Get the query params values, which don't change between calls and are hashable
+    # Lower case the list for set comparison with recipe ingredients list
+    query_params_ingredients_list = lc_list_of_ingredients(query_params_ingredients)
+    qp_ingredients_set = set(query_params_ingredients_list)
+
+    def get_percentage_matched(recipe_ingredients):
+        '''
+
+        :param recipe_ingredients(str), from the recipe search
+        :return: matched words(str), the percentage matched(float to 2 decimal places)
+        '''
+        recipe_ingredients_list = lc_list_of_ingredients(recipe_ingredients)
+        recipe_ingredients_set = set(recipe_ingredients_list)
+        # Get a set intersection of the two lower cased sets
+        matched_words_set = qp_ingredients_set.intersection(recipe_ingredients_set)
+        # Calculate the percentage of matches of matched words against the whole recipe ingredients keyword list
+        percentage_matched = (len(matched_words_set) / len(recipe_ingredients_set)) * 100
+        # Round to 2 decimal places
+        percentage_matched = round(percentage_matched, 2)
+        # return a string of sorted, space delimited matched words
+        matched_words_str = ', '.join(sorted(list(matched_words_set)))
+        return (matched_words_str, percentage_matched)
+
+    return get_percentage_matched
+
 class ProcessRecipeSearch(View):
     """ View for processing recipe search form
     """
@@ -34,10 +68,10 @@ class ProcessRecipeSearch(View):
 
         # Success: we have some results, so turn them into graph images for display
         # Create a gauge chart showing what percentage of ingredient keywords we matched, for each match
-        gauge_chart = self.make_gauge_chart(query_params, recipes)
+        gauge_chart = self._make_gauge_chart(query_params, recipes)
 
         # Create a pie chart showing how Elasticsearch rated our matching recipes
-        pie_chart = self.make_pie_chart(recipes)
+        pie_chart = self._make_pie_chart(recipes)
 
         # Populate context var for rendering, with the charts we've created
         context = {
@@ -48,7 +82,7 @@ class ProcessRecipeSearch(View):
                 }
         return render(request, 'recipes/search_results.html', context)
 
-    def make_gauge_chart(self, query_params, recipes):
+    def _make_gauge_chart(self, query_params, recipes):
         '''
 
         :param query_params(query dict):
@@ -56,7 +90,7 @@ class ProcessRecipeSearch(View):
         :return(obj): gauge_chart
         '''
 
-        get_percentage_matched = self._percentage_of_ingredients_matched(query_params['ingredients'])
+        get_percentage_matched = percentage_of_ingredients_matched(query_params['ingredients'])
         gauge_chart = pygal.SolidGauge(width=1300, height=1300, inner_radius=0.50, style=CleanStyle)
         percent_formatter = lambda x: '{:.10g}%'.format(x)
         gauge_chart.value_formatter = percent_formatter
@@ -80,7 +114,7 @@ class ProcessRecipeSearch(View):
 
         return gauge_chart
 
-    def make_pie_chart(self, recipes):
+    def _make_pie_chart(self, recipes):
         '''
 
         :param recipes(list of dicts of hits returned from search):
@@ -102,36 +136,3 @@ class ProcessRecipeSearch(View):
 
         return pie_chart
 
-    def _percentage_of_ingredients_matched(self, query_params_ingredients):
-        '''
-        Private method to calculate the percentage of ingredient keywords matched for each Elasticsearch hit.
-        Return a closure to cache some calculated values (from query params, which are the same for each call)
-        
-        :param query_params_ingredients: 
-        :return: get_percentage_matched (function)
-        '''
-
-        # Get the query params values, which don't change between calls and are hashable
-        # Lower case the list for set comparison with recipe ingredients list
-        query_params_ingredients_list = lc_list_of_ingredients(query_params_ingredients)
-        qp_ingredients_set = set(query_params_ingredients_list)
-
-        def get_percentage_matched(recipe_ingredients):
-            '''
-
-            :param recipe_ingredients(str), from the request query
-            :return: matched words(str), the percentage matched(float to 2 decimal places)
-            '''
-            recipe_ingredients_list = lc_list_of_ingredients(recipe_ingredients)
-            recipe_ingredients_set = set(recipe_ingredients_list)
-            # Get a set intersection of the two lower cased sets
-            matched_words_set = qp_ingredients_set.intersection(recipe_ingredients_set)
-            # Calculate the percentage of matches of matched words against the whole recipe ingredients keyword list
-            percentage_matched = (len(matched_words_set) / len(recipe_ingredients_set)) * 100
-            # Round to 2 decimal places
-            percentage_matched = round(percentage_matched, 2)
-            # return a string of sorted, space delimited matched words
-            matched_words_str = ', '.join(sorted(list(matched_words_set)))
-            return (matched_words_str, percentage_matched)
-
-        return get_percentage_matched
