7d463e0910dfe99e23383e1475bc07f1e3f13368
David Felce <davefelce@gmail.com>
2017-07-24 13:13:39 +0100

    hotfix: fixed json data and improved loader

--- scripts/json_loader.py
+++ scripts/json_loader.py
@@ -9,12 +9,13 @@ if __name__ == '__main__':
     json_files = [join(path, f) for f in listdir(path) if isfile(join(path, f))]
 
     for json_file in json_files:
+        print("DOING FILE: " + json_file)
         with open(json_file) as f:
             data = json.load(f)
 
         for row_dict in data:
             print("doing " + json.dumps(row_dict))
-            url = 'http://localhost:8002/recipes/list/'
+            url = 'http://localhost/recipes/list/'
             r = requests.post(url, json=row_dict)
             print(r.status_code)
 
